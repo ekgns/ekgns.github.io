befb0a4a55100f1318551885edf2ab26be6315fb
kimdahun.hello <kimdahun.hello@gmail.com>
2021-08-16 15:33:37 +0900

    첫 포스팅

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "변수&상수 ",
+        "excerpt":"상수 : 고정 된 수 사용방법 : 1. let (이름) : &lt;Type&gt; : 값 2. let (이름) = 값 ex) 1. let name : String = Bean (상수) (상수이름) : &lt;타입&gt; = 값 2. let name2 = Bean (상수) (상수이름) = 값 변수 : 변화가 가능한 수 사용방법 : var...","categories": ["Swift"],
+        "tags": ["Swift 기분문법"],
+        "url": "/swift/let&var/",
+        "teaser": null
+      }]
